4c33cb814e9567268d07d9c3f4b71e45ba38f03e
sanjith-s <sanjithsenthilmurugan@gmail.com>
2022-10-27 20:40:51 +0530

    Updated SignUp

--- src/signup.js
+++ src/signup.js
@@ -1,13 +1,26 @@
 // import logo from './logo.svg';
 // import './App.css';
-import { useState } from 'react';
+import { useState, useRef} from 'react';
 import validator from 'validator';
+import ReCAPTCHA from "react-google-recaptcha"
+
 
 function Signup() {
+
+  const captchaRef = useRef(null)
+
   const [signupdata, setsignupdata] = useState({
     email: "",
     password: "",
     confpass: "",
+    name: "",
+    phone: "",
+    aadhar: "",
+    addr1: "",
+    addr2: "",
+    city: "",
+    district: "",
+    pincode: "",
   });
 
 
@@ -17,6 +30,8 @@ function Signup() {
 
   const submit = (event) => {
     event.preventDefault();
+    const token = captchaRef.current.getValue();
+    captchaRef.current.reset();
     let emailChk = 0;
     let passChk = 0;
 
@@ -34,15 +49,25 @@ function Signup() {
     }
     
     alert('Validation successful')
+    console.log(token)
   }
 
   return (
     <div>
       <form onSubmit={submit} method='post'>
         <span>Signup</span><br/><br/>
+        <label>Name: <input type='text' name='name' value={signupdata.name} onChange={handleChange} /><br /><br /></label>
+        <label>Phone Number: <input type='text' name='phone' value={signupdata.phone} onChange={handleChange} /><br /><br /></label>
+        <label>Aadhaar Number: <input type='text' name='aadhar' value={signupdata.aadhar} onChange={handleChange} /><br /><br /></label>
+        <label>Address Line 1: <input type='text' name='addr1' value={signupdata.addr1} onChange={handleChange} /><br /><br /></label>
+        <label>Address Line 2: <input type='text' name='addr2' value={signupdata.addr2} onChange={handleChange} /><br /><br /></label>
+        <label>City/Town: <input type='text' name='city' value={signupdata.city} onChange={handleChange} /><br /><br /></label>
+        <label>District: <input type='text' name='district' value={signupdata.district} onChange={handleChange} /><br /><br /></label>
+        <label>Pincode: <input type='text' name='pincode' value={signupdata.pincode} onChange={handleChange} /><br /><br /></label>
         <label>Email: <input type='text' name='email' value={signupdata.email} onChange={handleChange} /><br /><br /></label>
         <label>Password: <input  type='text' name='password' value={signupdata.password} onChange={handleChange} /><br /><br /></label>
         <label>Confirm Password: <input type='text' name='confpass' value={signupdata.confpass} onChange={handleChange} /><br /><br /></label>
+        <ReCAPTCHA sitekey={process.env.REACT_APP_SITE_KEY} ref={captchaRef}/><br/>
         <button type='submit'>Submit</button>
       </form>
     </div>

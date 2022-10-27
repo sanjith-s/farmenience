2c6e1ffcad19061546137f232a411d7a4550e61c
sanjith-s <sanjithsenthilmurugan@gmail.com>
2022-10-27 21:37:56 +0530

    import Signup from './signup';

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import Signup from './signup';
+
+
+function App() {
+
+
+  return (
+    <div>
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import Signup from './signup';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Signup />
+    <App />
 );
 

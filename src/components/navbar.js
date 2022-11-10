f9381431839d9dcb97dd58f867acf10e8e8ce98b
Yuvaraj787 <yuvarajv787@gmail.com>
2022-11-10 20:47:22 +0530

    added page n6 with basic basign by yuvaraj

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './../css/navbar.css';
+function Navbar() {
+	return (
+     <div className="">
+      <div className="nav">
+         <span className="nav-brand">
+        FARMENiENCE
+         </span>
+        <span className="nav-items">Home</span>
+        <span className="nav-items">About Us</span>
+      </div>
+     </div>
+	)
+}
+export default Navbar;

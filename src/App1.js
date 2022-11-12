cf5c7023b575a40de85eb31024bbb63437f132a3
MettaSurendhar <msurendhar8815@gmail.com>
2022-11-12 13:16:45 +0530

    added required files to merge pageM2

---
+++ src/App1.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+function App1() {}
+
+export default App1;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import App from './App';
+import App from "./App";
+import App1 from "./App1";
 
+const root1 = ReactDOM.createRoot(document.getElementById("root1"));
+root1.render(<App />);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
-
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+root2.render(<App1 />);

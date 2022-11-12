8ef232b8b83d2c7b1bcc22fd53ebf86970b7b827
sanjith-s <sanjithsenthilmurugan@gmail.com>
2022-11-12 15:02:37 +0530

    Updates to App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Signup from './signup';
 import Navbar from './components/navbar';
 import AllRequest from './showAllRequest';
 import PageM2 from './pageM2';
+import App1 from './App1';
+
 function App() {
 
 
   return (
     <div>
-    <Navbar />
-    <AllRequest />
-    <PageM2 />
+    <App1 />
     </div>
   );
 }

957e4a6095c0734c458bf7955b7c368e62806961
sanjith-s <sanjithsenthilmurugan@gmail.com>
2022-11-22 00:03:33 +0530

    Add n5

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import {BrowserRouter,Route,Routes} from 'react-router-dom';
-import Navbar from "./components/navbar";
-import Signup from "./signup";
-import AllRequest from "./showAllRequest";
-import PageM1 from "./pageM1";
-import PageM3 from "./pageM3";
-import PageM4 from "./pageM4";
-import Footer from "./components/footer.js";
-<<<<<<< HEAD
-function App() {
-  const itemsBought = [
-=======
-import PageM5 from "./pageM5";
-import React from "react";
+
 // import Signup from "./signup";
 // import AllRequest from "./showAllRequest";
 // import PageM2 from "./pageM2";
@@ -33,7 +21,6 @@ function App() {
       {/* <PageM4 saleName="XXXXXX" orderDate="10/11/2022" deliveryDate="13/11/2022"  
       clientName="YYYYYY" paymentMode="By cash" transactionId="34232211" remarks="hhsckbhsbhsbj" 
       itemsBought={[
->>>>>>> 8512936ed72448772524c9e0730942e4ff4c1fd7
         {
           itemName:"Apple",
           quantity:4,
@@ -49,25 +36,9 @@ function App() {
           quantity:20,
           price:25
         }
-<<<<<<< HEAD
         ];
   return (
     <div>{/*---hr tag here is just to split two pages----*/}
-      <Navbar />
-      <Signup /><hr />
-      <AllRequest /><hr />
-      <PageM1 /><hr />
-      <PageM3 /><hr />
-     (PAGE M4)
-      <PageM4 saleName="XXXXXX" orderDate="10/11/2022" deliveryDate="13/11/2022"  
-      clientName="YYYYYY" paymentMode="By cash" transactionId="34232211" remarks="hhsckbhsbhsbj" 
-      itemsBought={itemsBought} /><hr />
-      <Footer />
-=======
-        ]} /><hr />
-      <Footer /> */}
->>>>>>> 8512936ed72448772524c9e0730942e4ff4c1fd7
-        ]} /> */}
       <PageN5/>
     </div>
   );

bab6a105328d5ba0ef3b0611b5a6018f93fdf42b
sanjith-s <sanjithsenthilmurugan@gmail.com>
2022-11-21 23:57:43 +0530

    Added N5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from "react";
-import Navbar from "./components/navbar";
-import Signup from "./signup";
-import AllRequest from "./showAllRequest";
-import PageM2 from "./pageM2";
-import PageM3 from "./pageM3";
-import PageM4 from "./pageM4";
-import Footer from "./components/footer.js";
-import PageM5 from "./pageM5";
+// import Signup from "./signup";
+// import AllRequest from "./showAllRequest";
+// import PageM2 from "./pageM2";
+// import PageM3 from "./pageM3";
+// import PageM4 from "./pageM4";
+// import Footer from "./components/footer.js";
+// import PageM5 from "./pageM5";
+import PageN5 from "./pageN5";
 
 function App() {
   return (
-    <div>{/*---hr tag here is just to split two pages----*/}
-      <Navbar />
-      <PageM5/>
+    <div>
+      {/* <PageM4 /> */}
       {/* <Signup /><hr /> */}
       {/* <AllRequest /><hr /> */}
      {/* <PageM2 /><hr /> */}
-      {/* <PageM3 /><hr />
-     (PAGE M4)
-      <PageM4 saleName="XXXXXX" orderDate="10/11/2022" deliveryDate="13/11/2022"  
+      {/* <PageM3 /><hr >*/}
+      {/* <PageM4 saleName="XXXXXX" orderDate="10/11/2022" deliveryDate="13/11/2022"  
       clientName="YYYYYY" paymentMode="By cash" transactionId="34232211" remarks="hhsckbhsbhsbj" 
       itemsBought={[
         {
@@ -36,8 +34,8 @@ function App() {
           quantity:20,
           price:25
         }
-        ]} /><hr />
-      <Footer /> */}
+        ]} /> */}
+      <PageN5/>
     </div>
   );
 }

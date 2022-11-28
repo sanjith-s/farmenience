a1846f0d9af91ecb001495d1e49e3dcb09b62ca2
Yuvaraj787 <yuvarajv787@gmail.com>
2022-11-29 00:29:46 +0530

    added pagem12 but not completed fully

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,16 @@ import PageN5 from "./pageN5";
 import PageM4 from "./pageM4";
 import PageM12 from "./pageM12";
 import PageM1 from './pageM1';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 function App() {
   return (
     <div>
-      <PageM12 />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PageM12 />} />
+          <Route path="/ordersummary" />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pageM12.jsx
+++ src/pageM12.jsx
@@ -2,10 +2,13 @@ import React,{useState} from 'react';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import './css/pageM12.css';
 import ShareIcon from '@mui/icons-material/Share';
+import * as Scroll from 'react-scroll';
+import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 import DeleteIcon from '@mui/icons-material/Delete';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import ItemBox from './components/itemBox';
+import ProcessBar from './components/process_bar';
 function Fname() {
   var username = "Yuvaraj";
   var [Address,setAddress] = useState("Ceg-chennai");
@@ -20,6 +23,18 @@ function Fname() {
   }
   const handleAddress = () => {
     setAddress(document.querySelector(".add-in").value);
+  }
+  var [process_no,setProcess] = useState([1,'10']);
+
+  const placeOrder = () => {
+    setProcess([2,'55']);
+  }
+  var dates = [];
+  var curdate = new Date();
+  for (var i = 0;i <= 6;i++) {
+  var thatDate = new Date();
+  thatDate.setDate(curdate.getDate()+i);
+  dates.push(thatDate);
   }
   const itemsAdded = [
    {
@@ -43,11 +58,22 @@ function Fname() {
   return (
      <div className="paymentpage">
          <div className="pay-details">
+         <Element className="page1" id="page1">
              <div className="pay-nav">
                 <div className="cart">
                     <div><a className="cart-ele1 back-btn"><KeyboardBackspaceIcon className="back-icon" fontSize="large" /></a></div>     
                     <div className="cart-ele2"><h1 className="cart-text">My Cart</h1></div>
                     <ShoppingCartIcon fontSize="large" className="cart-icon" />
+                </div>
+             </div>
+             <div className="pro-bar">
+                <div className="progress-title">
+                   <div className="p-items"><span>Address Confirmation</span></div>
+                   <div className="p-items"><span>Order Summary</span></div>
+                   <div className="p-items"><span>Payment</span></div>
+                </div>
+                <div className="progress-bar">
+                   <ProcessBar bgcolor="blue" progress={process_no[1]}  height={7} />
                 </div>
              </div>
              <div className="address-box">
@@ -75,15 +101,52 @@ function Fname() {
              </div><br />
              <h1 className="bot-head">Price Details</h1><br />
              <div className="table1">
-               <table border="2">
-                       <tr><td>S.no</td><td>Item</td><td>Quantity</td><td>Price</td><td>Discount</td><td>Delivery Fee</td><td>Amount</td></tr>
+                 <div><table border="2">
+                       <tr className="bold"><td>S.no</td><td>Item</td><td>Quantity</td><td>Price</td><td>Discount</td><td>Delivery Fee</td><td>Amount</td></tr>
                        {itemsAdded.map((item,index)=>{
                         return <tr><td>{index+1}</td><td>{item.iName}</td><td>{item.quantity}</td><td> ₹ {item.price}</td><td>₹ {item.discount}</td><td>₹ 50</td><td>₹ {item.price + 50 - item.discount}</td></tr>
                        })
                        }
-                       <tr><td colspan="6"><strong>Total Amount </strong></td><td>₹ {total_amount}</td></tr>
+                       <tr className="bold"><td colspan="6"><strong>Total Amount </strong></td><td>₹ {total_amount}</td></tr>
                       </table>
+                 </div>
+              <div className="order-row"><div><Link to="order-summary" smooth={true} duration={500} className="order-btn" onClick={placeOrder}>Place Order</Link></div></div>
+
              </div>
+             </Element>
+             <Element className="order-summary" id="order-summary">
+              <div className="orderSummary">
+                
+                <div className="address-box">
+                <div className="del-address">
+                    <div className="add1">
+                        <div className="top top1"><span>Deliver to : </span></div>
+                        <div className="ans ans1"><span>{username}</span></div>
+                    </div>
+                    <div className="add2">
+                        <div className="top top2"><span>Delivery Address :</span></div>
+                        <div className=" ans ans2"><span>{Address}</span></div>
+                    </div>
+                </div>
+                </div>
+                <p  className="ex-text bot-head">Choose Your Preffered Date for the Delivery</p><br />
+                <div className="del-slot">
+                  {dates.map(date=>{
+                    return <div className="d-box">
+                       <p>{date.toLocaleDateString()}</p>
+                       <input type="radio" name="date-opt" className="date-option" />                      
+                    </div>
+                  })}
+                </div>
+                <div className="time-slot" style={{textAlign:"center"}}>
+                <div style={{textAlign:"left",display:"inline-block"}}>
+                 <p className="ex-text">Choose Your Preffered Time slot</p>
+                  <input type="radio" name="time-opt" /><span>10.00 AM - 1.30 PM</span><br/>
+                  <input type="radio" name="time-opt" /><span>1.30 PM - 7.00 PM</span>
+                </div>
+                </div>
+               </div>
+             </Element>
          </div>
      </div>
   )

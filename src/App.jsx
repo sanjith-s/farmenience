6bb17f2d216e0f305d2656ad59ded3afd4165c3d
MettaSurendhar <msurendhar8815@gmail.com>
2022-11-29 20:49:17 +0530

    updated the main branch with all the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import PageM1 from './pageM1';
-import PageM3 from './pageM3';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PageM1 from "./pageM1";
+import PageM3 from "./pageM3";
 import PageM4 from "./pageM4";
 import PageM5 from "./pageM5";
 import PageM12 from "./pageM12";
@@ -12,7 +12,27 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<PageM1 />} />
+          <Route path="/ordersummary" />
+        </Routes>
+        <Routes>
+          <Route path="/" element={<PageM3 />} />
+          <Route path="/ordersummary" />
+        </Routes>
+        {/* <Routes>
+          <Route path="/" element={<PageM4 />} />
+          <Route path="/ordersummary" />
+        </Routes> */}
+        {/* <Routes>
+          <Route path="/" element={<PageM5 />} />
+          <Route path="/ordersummary" />
+        </Routes> */}
+        <Routes>
           <Route path="/" element={<PageM12 />} />
+          <Route path="/ordersummary" />
+        </Routes>
+        <Routes>
+          <Route path="/" element={<PageN5 />} />
           <Route path="/ordersummary" />
         </Routes>
       </BrowserRouter>

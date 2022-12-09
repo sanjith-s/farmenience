b9be32a4e99652b62132dc337e9866aa6ccd3dbc
harshit0902 <83470228+harshit0902@users.noreply.github.com>
2022-12-10 01:02:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import PageM1 from './pageM1';
 import PageM3 from './pageM3';
 import PageM4 from "./pageM4";
 import PageM5 from "./pageM5";
+import PageM11 from "./pageM11";
 import PageM12 from "./pageM12";
 import PageM17 from "./pageM17";
 import PageN5 from "./pageN5";
-import PageM11 from "./pageM11";
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 

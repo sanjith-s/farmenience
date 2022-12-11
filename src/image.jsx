8f1e4504d27b4124e4f7708c3367be278beacedb
harshit0902 <83470228+harshit0902@users.noreply.github.com>
2022-12-12 01:19:12 +0530

    Update image.jsx

--- src/image.jsx
+++ src/image.jsx
@@ -2,22 +2,30 @@ import React from "react";
 import "./css/pageM18.css";
 
 
-const PageM18 = () => {
+function Image() {
 
-  return (
-    <div>
-      <div>
-        <span className="title">Upload file</span>
-      </div>
-      <br></br><br></br><br></br><br></br>
-      <div>
-      <input type="file" placeholder='Enter value'
-          onChange={(event) => { setQty(event.target.value); } }></input>
-      </div>
-      <center><button class="buttons">Submit Request</button></center>
-      <br></br><br></br>
-    </div>
-  );
+    const [file, setFile] = useState("");
+
+    const SubmitLogin = () => {
+        Axios.post('http://localhost:8080/upload').then((response) => {
+            File: file
+        });
+    }
+
+    return (
+        <div>
+            <div>
+                <span className="title">Upload file</span>
+            </div>
+            <br></br><br></br><br></br><br></br>
+            <div>
+                <input type="file" placeholder='Enter value'
+                    onChange={(event) => { setFile(event.target.value); }}></input>
+            </div>
+            <center><button class="buttons" onClick={SubmitLogin}>Submit Request</button></center>
+            <br></br><br></br>
+        </div>
+    );
 };
 
-export default PageM18;
+export default Image;

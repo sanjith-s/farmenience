0d01d0672df280a459617e7da9dfefa4371e2183
karthiksenthil28 <student.mail.karthik@gmail.com>
2022-12-11 19:15:29 +0530

    Page M19

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import PageM11 from "./pageM11";
 import PageM12 from "./pageM12";
 import PageM17 from "./pageM17";
 import PageM18 from "./pageM18";
+import PageM19 from './pageM19';
 import PageN5 from "./pageN5";
 import Navbar from './components/navbar';
 import Footer from './components/footer';
@@ -20,8 +21,10 @@ function App() {
         <Routes>
           <Route path="/M3" element={<PageM3/>} />  
           <Route path="/M11" element={<PageM11/>} />
+          <Route path="/M5" element={<PageM5/>} />
           <Route path="/M17" element={<PageM17 />} />
           <Route path="/M18" element={<PageM18 />} />
+          <Route path="/M19" element={<PageM19/>} />
           <Route path="/ordersummary" />
         </Routes>
       </BrowserRouter>

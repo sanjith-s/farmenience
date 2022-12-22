61cf2149654553b6eb43576d0cd72e8fbb4d98df
harshit0902 <83470228+harshit0902@users.noreply.github.com>
2022-12-22 20:41:01 +0530

    Update cropRecommendation.jsx

--- src/cropRecommendation.jsx
+++ src/cropRecommendation.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Axios from "axios"
 import { useState, useRef } from "react";
 
 import CssBaseline from "@mui/material/CssBaseline";
@@ -20,11 +20,109 @@ import Divider from "@mui/material/Divider";
 import ArrowBackOutlinedIcon from "@mui/icons-material/ArrowBackOutlined";
 import ArrowForwardOutlinedIcon from "@mui/icons-material/ArrowForwardOutlined";
 
-import Crop from "./components/cropML";
+//import Crop from "./components/cropML";
 
 const PageN2 = () => {
 
-  
+  const [nitrogen, setNitrogen] = useState(0);
+  const [phosphorous, setPhosphorous] = useState(0);
+  const [potassium, setPotassium] = useState(0);
+  const [temp, setTemp] = useState(0);
+  const [humidity, setHumidity] = useState(0);
+  const [ph, setPh] = useState(0);
+  const [rainfall, setRainfall] = useState(0);
+
+  function CheckNitrogen() {
+    if(nitrogen >= 0 && nitrogen <= 100) {
+      CheckPhosphorous();
+    }
+
+    else {
+      alert("Nitrogen value is invalid !");
+    }
+  }
+
+  function CheckPhosphorous() {
+    if(phosphorous >= 0 && phosphorous <= 100) {
+      CheckPotassium();
+    }
+
+    else {
+      alert("Phosphorous value is invalid !");
+    }
+  }
+
+  function CheckPotassium() {
+    if(potassium >= 0 && potassium <= 100) {
+      CheckTemp();
+    }
+
+    else {
+      alert("Potassium value is invalid !");
+    }
+  }
+
+  function CheckTemp() {
+    if(temp >= 0 && temp <= 50) {
+      CheckHumidity();
+    }
+
+    else {
+      alert("Temperature value is invalid !");
+    }
+  }
+
+  function CheckHumidity() {
+    if(humidity >= 0 && nitrogen <= 100) {
+      CheckPH();
+    }
+
+    else {
+      alert("Humidity value is invalid !");
+    }
+  }
+
+  function CheckPH() {
+    if(ph >= 0 && ph <= 14) {
+      CheckRainfall();
+    }
+
+    else {
+      alert("pH value is invalid !");
+    }
+  }
+
+  function CheckRainfall() {
+    if(rainfall >= 0 && rainfall <= 1000) {
+      PredictCrop();
+    }
+
+    else {
+      alert("Rainfall value is invalid !");
+    }
+  }
+
+  function PredictCrop() {
+    Axios.post("http://localhost:5000/ml_model/crop_recomendation", {
+        N: nitrogen,
+        P: phosphorous,
+        K: potassium,
+        temperature: temp,
+        humidity: humidity,
+        ph: ph,
+        rainfall: rainfall,
+      })
+        .then((response) => {
+          // if (response.data.message == "Success") {
+          //   navigate("/login");
+          // }
+
+          alert(response.data)
+        })
+        .catch((res, err) => {
+          alert(res.response.data.message);
+        });
+  }
 
   return (
     <div style={{ boxSizing: "borderBox", padding: "20px" }}>
@@ -48,11 +146,126 @@ const PageN2 = () => {
             //overflow: "auto"
           }}
         >
-          <Crop />
+          <Stack
+            direction="row"
+            sx={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+          </Stack>
+          <TextField
+            id="filled-basic"
+            type="number"
+            label="Nitrogen"
+            variant="filled"
+            color="success"
+            onChange={(e) => { setNitrogen(e.target.value) }}
+            sx={{
+              backgroundColor: "#C4E1C5",
+              borderBottomColor: "black",
+              width: "70%",
+            }}
+          />
+          <br />
+          <TextField
+            id="filled-basic"
+            type="number"
+            label="Phosphorus"
+            variant="filled"
+            color="success"
+            onChange={(e) => { setPhosphorous(e.target.value) }}
+            sx={{
+              backgroundColor: "#C4E1C5",
+              borderBottomColor: "black",
+              width: "70%",
+            }}
+          />
+          <br />
+          <TextField
+            id="filled-basic"
+            type="number"
+            label="Potassium"
+            variant="filled"
+            color="success"
+            onChange={(e) => { setPotassium(e.target.value) }}
+            sx={{
+              backgroundColor: "#C4E1C5",
+              borderBottomColor: "black",
+              width: "70%",
+            }}
+          />
+          <br />
+          <TextField
+            id="filled-basic"
+            type="number"
+            label="Temperature (in °C)"
+            variant="filled"
+            color="success"
+            onChange={(e) => { setTemp(e.target.value) }}
+            sx={{
+              backgroundColor: "#C4E1C5",
+              borderBottomColor: "black",
+              width: "70%",
+            }}
+          />
+          <br />
+          <TextField
+            id="filled-basic"
+            type="number"
+            label="Humidity (in %)"
+            variant="filled"
+            color="success"
+            onChange={(e) => { setHumidity(e.target.value) }}
+            sx={{
+              backgroundColor: "#C4E1C5",
+              borderBottomColor: "black",
+              width: "70%",
+            }}
+          />
+          <br />
+          <TextField
+            id="filled-basic"
+            type="number"
+            label="ph Value of Soil"
+            variant="filled"
+            color="success"
+            onChange={(e) => { setPh(e.target.value) }}
+            sx={{
+              backgroundColor: "#C4E1C5",
+              borderBottomColor: "black",
+              width: "70%",
+            }}
+          />
+          <br />
+          <TextField
+            id="filled-basic"
+            type="number"
+            label="Rainfall (in mm)"
+            variant="filled"
+            color="success"
+            onChange={(e) => { setRainfall(e.target.value) }}
+            sx={{
+              backgroundColor: "#C4E1C5",
+              borderBottomColor: "black",
+              width: "70%",
+            }}
+          />
+          <br />
+          <Stack
+            direction="row"
+            divider={<Divider orientation="vertical" flexItem />}
+            spacing={2}
+            display="flex"
+            justifyContent="center"
+          >
+          </Stack>
         </Container>
       </Stack>
       <Box textAlign="center" padding={"200px 250px 50px 250px"}>
-        <Button variant="contained" sx={{ bgcolor: "#1FE57A", margin: "auto" }}>
+        <Button variant="contained" onClick={CheckNitrogen()} sx={{ bgcolor: "#1FE57A", margin: "auto" }}>
           Predict
         </Button>
       </Box>

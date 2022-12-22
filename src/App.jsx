35a8180310baca548487379c1a49818d203a04ca
harshit0902 <83470228+harshit0902@users.noreply.github.com>
2022-12-22 16:20:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,12 @@ import PageM16 from "./pageM16";
 import PageM17 from "./pageM17";
 import PageM18 from "./pageM18";
 import PageM19 from "./pageM19";
-import AllQueries from "./N1";
+//import AllQueries from "./N1";
+import PageN1 from "./pageN1";
 import PageN2 from "./pageN2a";
 import PageN4 from "./pageN4";
 import PageN5 from "./pageN5";
 import CropRecommendation from "./cropRecommendation";
-import PageN1 from "./pageN1";
 
 function App() {
   return (
@@ -38,26 +38,27 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Homepage1 />} />
+            <Route path="/homepage2" element={<Homepage2 />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
             <Route path="/M1" element={<PageM1 />} />
             <Route path="/M3" element={<PageM3 />} />
             <Route path="/M4" element={<PageM4 />} />
             <Route path="/M5" element={<PageM5 />} />
-            <Route path="/M9" element={<PageM9 />} />
-            <Route path="/N4" element={<PageN4 />} />
-            <Route path="/M11" element={<PageM11 />} />
             <Route path="/M6" element={<PageM6 />} />
-            <Route path="/M16" element={<PageM16 />} />
+            <Route path="/M9" element={<PageM9 />} />
+            <Route path="/M11" element={<PageM11 />} />
             <Route path="/M15" element={<PageM15 />} />
+            <Route path="/M16" element={<PageM16 />} />
             <Route path="/M17" element={<PageM17 />} />
             <Route path="/M18" element={<PageM18 />} />
             <Route path="/M19" element={<PageM19 />} />
             <Route path="/M10" element={<PageM10a />} />
+            <Route path="/N1" element={<PageN1 />} />
             <Route path="/N2a" element={<PageN2 />} />
+            <Route path="/N4" element={<PageN4 />} />
             <Route path="/N5" element={<PageN5 />} />
             <Route path="/cropRecommendation" element={<CropRecommendation />} />
-            <Route path="/N1" element={<PageN1/>}/>
             <Route path="/ordersummary" />
           </Routes>
         </BrowserRouter>

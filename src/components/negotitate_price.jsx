754c25fcc55937cedf06c9ec00ba9d4ff7ef2ee5
MettaSurendhar <msurendhar8815@gmail.com>
2022-12-29 23:22:50 +0530

    updated mui to pagem16 and its components

--- src/components/negotitate_price.jsx
+++ src/components/negotitate_price.jsx
@@ -1,125 +1,201 @@
-import { useState } from 'react';
-import '../css/pageM16.css';
-import Typography from '@mui/material/Typography';
-import Card from '@mui/material/Card';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import React, { useState } from "react";
+import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+  CardActions,
+  CardContent,
+  Typography,
+  Card,
+  Stack,
+} from "@mui/material";
 
-const Negotiate = (props) =>{
-    const [price,setPrice]=useState(0);
-    const displayPrice = ()=>{
-        alert('The price is '+price)
-    }
-    return (
-        <>
-        <Card id="main-com">
-            <div id="left">
-                <table id="left-content">
-                    <tr>
-                        <td id="left-table">
-                            <Typography color="initial" id="left-table">
-                                Product Name
-                            </Typography>    
-                        </td>
-                        <td id="right-table">
-                            <Typography color="initial" id="right-table">
-                                : {props.Name}  
-                            </Typography> 
-                        </td>
-                    </tr>
-                    <tr>
-                        <td id="left-table">
-                            <Typography color="initial" id="left-table">
-                                Quantity
-                            </Typography> 
-                        </td>
-                        <td id="right-table">
-                            <Typography color="initial" id="right-table">
-                                : {props.Quantity}  
-                            </Typography> 
-                        </td>
-                    </tr>
-                    <tr>
-                        <td id="left-table">
-                            <Typography color="initial" id="left-table">
-                                Specific Type
-                            </Typography> 
-                        </td>
-                        <td id="right-table">
-                            <Typography color="initial" id="right-table">
-                                : {props.Type}  
-                            </Typography> 
-                        </td>
-                    </tr>
-                    <tr>
-                        <td id="left-table">     
-                            <Typography color="initial" id="left-table">
-                                Buyer name
-                            </Typography> 
-                        </td>
-                        <td id="right-table">
-                            <Typography color="initial" id="right-table">
-                                : {props.Buyer}  
-                            </Typography> 
-                        </td>
-                    </tr>
-                    <tr>
-                        <td id="left-table"> 
-                            <Typography color="initial" id="left-table">
-                                Buyer Address
-                            </Typography>
-                        </td>
-                        <td id="right-table">
-                            <Typography color="initial" id="right-table">
-                                : {props.Address}  
-                            </Typography> 
-                        </td>
-                    </tr>
-                </table>
-            </div>
-            <div id="right">
-                <table id="left-content">
-                        <tr>
-                            <td id="left-table">                              
-                                <Typography color="initial" id="left-table">
-                                    Pricings
-                                </Typography>     
-                            </td>
-                            <td id="right-table">
-                                <Typography color="initial" id="right-table">
-                                    :  {new Intl.NumberFormat('en-IN').format(props.Price)} INR 
-                                </Typography> 
-                            </td>
-                        </tr>
-                </table>
-                <Card id="price-entry">
-                    <Typography variant='h5' color="initial" id="price-entry-title">
-                        Enter the price
-                    </Typography> 
-                    <table id="price-entry-box">
-                        <tr>
-                            <td id="left-table">
-                                <TextField  label="Enter Price" variant="standard" type="number"
-                                    onChange={(e)=>{setPrice(e.target.value)}}>
-                                </TextField>
-                            </td>
-                            <td id="right-table-inr">
-                                <Typography color="initial"  id="inr-style">
-                                    INR
-                                </Typography> 
-                            </td>
-                        </tr>
-                    </table>
-                    <center>
-                        <Button variant="contained" id="price-submit" onClick={()=>{displayPrice();}}>
-                            Submit
-                        </Button>
-                    </center>
-                </Card>
-            </div>
-        </Card>
-        </>
-    )
-}
+const itemsName = [
+  "product name",
+  "quantity",
+  "specific type",
+  "buyer name",
+  "buyer address",
+];
+
+const Negotiate = (props) => {
+  const [price, setPrice] = useState(0);
+  const displayPrice = () => {
+    alert("The price is " + price);
+  };
+
+  const itemsValue = [
+    props.name,
+    props.quantity,
+    props.type,
+    props.buyer,
+    props.address,
+  ];
+
+  return (
+    <Card
+      style={{
+        margin: "60px",
+        marginTop: "0px",
+        display: "flex",
+        justifyContent: "center",
+        padding: "50px 0px",
+        borderRadius: "12px",
+        columnGap: "50px",
+        backgroundColor: "#eee",
+      }}
+    >
+      <Box
+        sx={{
+          width: "400px",
+          borderRadius: "8px",
+          border: "2px solid #000",
+          backgroundColor: "#fff",
+        }}
+      >
+        <CardContent style={{ padding: "15px 25px" }}>
+          <Stack>
+            {itemsName.map((value, index) => {
+              return (
+                <Box
+                  key={index}
+                  sx={{
+                    display: "flex",
+                    columnGap: "20px",
+                    justifyContent: "space-between",
+                    height: "50px",
+                    alignItems: "center",
+                    margin: "2px 0px",
+                  }}
+                >
+                  <Typography
+                    style={{
+                      fontWeight: "600",
+                      fontSize: "20px",
+                      textTransform: "uppercase",
+                    }}
+                  >
+                    {value}
+                  </Typography>
+                  <Typography variant="h6">{itemsValue[index]}</Typography>
+                </Box>
+              );
+            })}
+          </Stack>
+        </CardContent>
+      </Box>
+      <CardActions
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          rowGap: "30px",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "40%",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            columnGap: "50px",
+            justifyContent: "space-between",
+            height: "50px",
+            alignItems: "center",
+            margin: "2px 0px",
+            borderBottom: "2px solid",
+          }}
+        >
+          <Typography
+            style={{
+              fontWeight: "600",
+              fontSize: "22px",
+              textTransform: "uppercase",
+            }}
+          >
+            pricings
+          </Typography>
+          <Typography variant="h6">{props.price}</Typography>
+        </Box>
+        <Box
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            rowGap: "40px",
+            backgroundColor: "#fff",
+            padding: "20px",
+            borderRadius: "8px",
+            border: "2px solid #888",
+          }}
+        >
+          <Typography
+            color="initial"
+            id="price-entry-title"
+            style={{
+              textAlign: "center",
+              textTransform: "uppercase",
+              fontSize: "28px",
+            }}
+          >
+            Enter the price
+          </Typography>
+
+          <Box
+            sx={{
+              padding: "20px",
+              display: "flex",
+              columnGap: "20px",
+              alignItems: "center",
+              borderRadius: "4px",
+              backgroundColor: "white",
+            }}
+          >
+            <FormControl
+              style={{
+                position: "sticky",
+                borderRadius: "4px",
+              }}
+              onChange={(e) => {
+                setPrice(e.target.value);
+              }}
+            >
+              <InputLabel htmlFor="outlined-adornment-amount">
+                Amount
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-amount"
+                type="amount"
+                min={props.actualPrice / 2}
+                max={props.actualPrice / 2 + props.actualPrice}
+                message="hello"
+                placeholder={props.discountPrice}
+                startAdornment={
+                  <InputAdornment position="start">
+                    <CurrencyRupeeIcon />
+                  </InputAdornment>
+                }
+                label="Amount"
+              />
+            </FormControl>
+
+            <IconButton>
+              <ThumbUpIcon
+                variant="contained"
+                onClick={() => {
+                  displayPrice();
+                }}
+                style={{ color: "green" }}
+              />
+            </IconButton>
+          </Box>
+        </Box>
+      </CardActions>
+    </Card>
+  );
+};
 
 export default Negotiate;
--- src/pageM16.jsx
+++ src/pageM16.jsx
@@ -1,43 +1,59 @@
-import './css/pageM16.css'
-import Negotiate from './components/negotitate_price'
-import { Typography } from '@material-ui/core'
+import React from "react";
+import Negotiate from "./components/negotitate_price";
+import { Typography } from "@material-ui/core";
+import { Box } from "@mui/material";
 
+const content = [
+  {
+    Name: "Product 1",
+    Quantity: 24,
+    Type: "Type 1",
+    Buyer: "Harshit",
+    Address: "Chennai",
+    Price: 123456789,
+  },
+  {
+    Name: "Product 2",
+    Quantity: 40,
+    Type: "Type 2",
+    Buyer: "Sanjith",
+    Address: "Chennai",
+    Price: 425456789,
+  },
+];
 
 const PageM16 = () => {
-    let content=[
-        {
-            'Name': 'Product 1',
-            'Quantity': 24,
-            'Type': 'Type 1',
-            'Buyer': 'Harshit',
-            'Address': 'Chennai',
-            'Price': 123456789
-        },
-        {
-            'Name': 'Product 2',
-            'Quantity': 40,
-            'Type': 'Type 2',
-            'Buyer': 'Sanjith',
-            'Address': 'Chennai',
-            'Price': 425456789
-        }
-    ]
-    return (
-        <>
-            <Typography id="title" variant='h3'>
-                Negotiate Prices
-            </Typography>
-            {content.map((id,key)=>{
-                return (
-                    <>
-                        <Negotiate Name={id.Name} Price={id.Price} Quantity={id.Quantity}
-                        Buyer={id.Buyer} Type={id.Type} Address={id.Address} />
-                    </>
-                )
-            })}
+  return (
+    <Box>
+      <Typography
+        id="title"
+        style={{
+          textTransform: "uppercase",
+          textAlign: "center",
+          margin: "30px 0px",
+          fontWeight: "600px",
+          fontSize: "52px",
+        }}
+      >
+        Negotiate Prices
+      </Typography>
+      {content.map((id, key) => {
+        return (
+          <Box>
+            <Negotiate
+              key={key}
+              name={id.Name}
+              price={id.Price}
+              quantity={id.Quantity}
+              buyer={id.Buyer}
+              type={id.Type}
+              address={id.Address}
+            />
+          </Box>
+        );
+      })}
+    </Box>
+  );
+};
 
-        </>
-    )
-}
-
-export default PageM16
+export default PageM16;

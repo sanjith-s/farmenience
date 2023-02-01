529e3588acff3a404cedef3e8f13a3fa7d08e269
harshit0902 <83470228+harshit0902@users.noreply.github.com>
2023-02-01 10:25:13 +0530

    Creating page N11

---
+++ src/Ngo/pageN11.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import queryList from "../components/queryList";
+import { Box, Typography } from "@mui/material";
+import wheat from "../wheatimg.jpg";
+import carrot from "../carrot.jpg";
+
+const content = [
+  {
+    name: "Consumer name",
+    phno: 2484930933,
+    address: "no 323 , some street,  town, chennai-23232",
+    itemName: "wheat",
+    itemQuantity: "3 ",
+    itemPrice: 250,
+    regNo: "23.42.424",
+    img: wheat,
+  },
+  {
+    name: "Consumer name",
+    phno: 134132414,
+    address: "no 323 , some street,  some town, mumbai-23232",
+    itemName: "carrot",
+    itemQuantity: 3,
+    itemPrice: 350,
+    regNo: "23.42.425",
+    img: carrot,
+  },
+  {
+    name: "Consumer name",
+    phno: 2484930933,
+    address: "no 323 , some street,  town, chennai-23232",
+    itemName: "wheat",
+    itemQuantity: "3 ",
+    itemPrice: 250,
+    regNo: "23.42.424",
+    img: wheat,
+  },
+  {
+    name: "Consumer name",
+    phno: 134132414,
+    address: "no 323 , some street,  some town, mumbai-23232",
+    itemName: "carrot",
+    itemQuantity: 3,
+    itemPrice: 350,
+    regNo: "23.42.425",
+    img: carrot,
+  },
+];
+
+function PageN11() {
+  return (
+    <Box
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        margin: "20px 0px",
+        marginBottom: "40px",
+      }}
+    >
+      <Box sx={{ padding: "20px 0px" }}>
+        <Typography
+          variant="h4"
+          style={{ fontWeight: "600", textTransform: "uppercase" }}
+        >
+          you have {content.length} requests{" "}
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          width: "fit-content",
+          border: "5px solid",
+          borderRadius: "3px",
+          backgroundColor: "#bdfbbf",
+        }}
+      >
+        <Box
+          sx={{
+            padding: "40px",
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+            columnGap: "50px",
+            rowGap: "50px",
+          }}
+        >
+          {content.map((req, index) => {
+            return (
+              <queryList
+                key={index + 1}
+                reqId={req.regNo}
+                name={req.name}
+                phoneNo={req.phno}
+                itemName={req.itemName}
+                itemQuantity={req.itemQuantity}
+                data={req}
+              />
+            );
+          })}
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+export default PageN11;
---
+++ src/components/queryList.jsx
@@ -0,0 +1,236 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@mui/material";
+
+function Query(props) {
+  const viewRequest = () => {
+    const [buyerRequestDetails, setBuyerRequestDetails] = useState([]);
+
+    Axios.post("http://localhost:9091/api/admin/deleteitem", {
+      itemID: id,
+    }).then((response) => {
+      console.log(response);
+      if (response.data == "success") {
+        setBuyerRequestDetails(response.data);
+        navigate("/pageM3");
+      }
+    });
+  };
+
+  return (
+    <Card
+      style={{
+        padding: "15px",
+        width: "450px",
+        borderRadius: "5px",
+        border: "2px solid #000000",
+      }}
+    >
+      <CardContent
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "flex-start",
+          padding: "10px 5px",
+          borderTop: "2px solid",
+          columnGap: "20px",
+        }}
+      >
+        <Typography
+          style={{
+            width: "45%",
+            padding: "5px 5px 5px 10px",
+            backgroundColor: "#16e575",
+            borderRadius: "3px",
+            fontWeight: "600",
+            textTransform: "uppercase",
+          }}
+        >
+          Request ID
+        </Typography>
+        <Typography
+          style={{
+            width: "55%",
+            padding: "5px 5px 5px 10px",
+            backgroundColor: "#c4e1c5",
+            borderRadius: "3px",
+          }}
+        >
+          {props.reqId}
+        </Typography>
+      </CardContent>
+      <CardContent
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          padding: "10px 5px",
+          columnGap: "20px",
+        }}
+      >
+        <Typography
+          style={{
+            width: "45%",
+            padding: "5px 5px 5px 10px",
+            backgroundColor: "#16e575",
+            borderRadius: "3px",
+            fontWeight: "600",
+            textTransform: "uppercase",
+          }}
+        >
+          Name
+        </Typography>
+        <Typography
+          style={{
+            width: "55%",
+            paddingLeft: "10px",
+            padding: "5px 5px 5px 10px",
+            backgroundColor: "#c4e1c5",
+            borderRadius: "3px",
+          }}
+        >
+          {props.name}
+        </Typography>
+      </CardContent>
+      <CardContent
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          padding: "10px 5px",
+          columnGap: "20px",
+        }}
+      >
+        <Typography
+          style={{
+            width: "45%",
+            padding: "5px 5px 5px 10px",
+            backgroundColor: "#16e575",
+            borderRadius: "3px",
+            fontWeight: "600",
+            textTransform: "uppercase",
+          }}
+        >
+          Phone Number
+        </Typography>
+        <Typography
+          style={{
+            width: "55%",
+            paddingLeft: "10px",
+            padding: "5px 5px 5px 10px",
+            backgroundColor: "#c4e1c5",
+            borderRadius: "3px",
+          }}
+        >
+          {props.phoneNo}
+        </Typography>
+      </CardContent>
+      <CardContent
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          padding: "10px 5px",
+          columnGap: "20px",
+        }}
+      >
+        <Typography
+          style={{
+            width: "45%",
+            padding: "5px 5px 5px 10px",
+            backgroundColor: "#16e575",
+            borderRadius: "3px",
+            fontWeight: "600",
+            textTransform: "uppercase",
+          }}
+        >
+          Item Name
+        </Typography>
+        <Typography
+          style={{
+            width: "55%",
+            paddingLeft: "10px",
+            padding: "5px 5px 5px 10px",
+            backgroundColor: "#c4e1c5",
+            borderRadius: "3px",
+          }}
+        >
+          {props.itemName}
+        </Typography>
+      </CardContent>
+      <CardContent
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          columnGap: "20px",
+          padding: "10px 5px",
+          borderBottom: "2px solid",
+        }}
+      >
+        <Typography
+          style={{
+            width: "45%",
+            backgroundColor: "#16e575",
+            padding: "5px 5px 5px 10px",
+            borderRadius: "3px",
+            fontWeight: "600",
+            textTransform: "uppercase",
+          }}
+        >
+          Quantity
+        </Typography>
+        <Typography
+          style={{
+            width: "55%",
+            paddingLeft: "10px",
+            padding: "5px 5px 5px 10px",
+            backgroundColor: "#c4e1c5",
+            borderRadius: "3px",
+          }}
+        >
+          {props.itemQuantity}
+        </Typography>
+      </CardContent>
+      <CardActions
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Button
+          style={{
+            backgroundColor: "green",
+            border: "2px solid #000000",
+            marginTop: "15px",
+          }}
+          onClick={viewRequest}
+        >
+          <Link
+            to="/M2"
+            state={{
+              from: "Request details",
+              data: props.data,
+            }}
+            style={{ textDecoration: "none" }}
+          >
+            <Typography
+              style={{
+                color: "#ffffff",
+                fontWeight: "600",
+                fontSize: "16px",
+              }}
+            >
+              view request
+            </Typography>
+          </Link>
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+export default Query;

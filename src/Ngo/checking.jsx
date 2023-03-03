2b9ef3a80140fbddf04362a932c1ada4ee093e28
YUVARAJ787 <yuvarajv787@gmail.com>
2023-03-03 18:32:41 +0530

    Added styles and animation to page n10

--- src/Ngo/checking.jsx
+++ src/Ngo/checking.jsx
@@ -6,6 +6,93 @@ import Box from '@mui/material/Box';
 import AspectRatio from '@mui/joy/AspectRatio';
 import "./Pagen10.css";
 import { Typography } from '@mui/material';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import { Stack } from '@mui/system';
+
+
+
+
+
+
+
+
+
+
+
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+  
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+  
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+  const queries = [
+    "Fund Request by Farmer jon ...",
+    "Land Assistance by Farmer kesan ...",
+    "Fund extension requestt by farmer..",
+    "","","","","",""
+  ]
+ function BasicTabs() {
+    const [value, setValue] = React.useState(0);
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+  
+    return (
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+            <Tab label="Latest Queries" {...a11yProps(0)} />
+            <Tab label="Upcoming Appointments" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+            <div className='shower'>
+            {queries.map(que=>{
+            return (<div className='texts-box'>
+            <span className='b-text'>{que}</span>
+            </div>)
+            })}
+            </div>
+        </TabPanel>
+      </Box>
+    );
+  }
+
+
+
+
+
+
+
 
 function OneBox(props) {
     return (
@@ -57,6 +144,9 @@ export default function HomePage() {
                     </Grid>
                     <Grid item xs={3} className="box2">
                         <OneBox name={"View Queries"}/>
+                    </Grid>
+                    <Grid item xs={4}>
+                       <BasicTabs />
                     </Grid>
                 </Grid>
             </Box>

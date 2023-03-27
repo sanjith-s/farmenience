24809dadc75800a5f2b46ff3d8dd29cd8a19d60d
harshit0902 <83470228+harshit0902@users.noreply.github.com>
2023-03-27 12:47:45 +0530

    Axios Calls

--- src/Web1a.jsx
+++ src/Web1a.jsx
@@ -5,32 +5,78 @@ import { useNavigate } from "react-router-dom";
 import Axios from "axios";
 import Swal from 'sweetalert2';
 
-const Web1a = () =>{
-    const [data,setData]=useState({});
-    useEffect(() => {
-        Axios.get(`${baseURL}/ml_model/webscrapping`).
+const Web1a = () => {
+    const [data1, setData1] = useState({});
+    const [data2, setData2] = useState({});
+    const [data3, setData3] = useState({});
+    const [data4, setData4] = useState({});
+
+    const AxiosCalls = () => {
+        Axios.get(`${baseURL}/ml_model/webscrapping1`).
             then((response) => {
-                setData(response.data);
+                setData1(response.data);
             })
             .catch(async (res) => {
-                    await Swal.fire({
-                        icon: 'error',
-                        title: 'Oops...',
-                        text: 'Error',
-                    })
+                await Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Error',
+                })
             })
+
+        Axios.get(`${baseURL}/ml_model/webscrapping2`).
+            then((response) => {
+                setData2(response.data);
+            })
+            .catch(async (res) => {
+                await Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Error',
+                })
+            })
+
+        Axios.get(`${baseURL}/ml_model/webscrapping3`).
+            then((response) => {
+                setData3(response.data);
+            })
+            .catch(async (res) => {
+                await Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Error',
+                })
+            })
+
+        Axios.get(`${baseURL}/ml_model/webscrapping4`).
+            then((response) => {
+                setData4(response.data);
+            })
+            .catch(async (res) => {
+                await Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Error',
+                })
+            })
+    }
+
+    useEffect(async () => {
+        await AxiosCalls()
     }, []);
+
+
     return (
         <>
             {JSON.stringify(data)}
             <br></br>
-            {data['Avarai (1 Kg)']}
+            {data1['Avarai (1 Kg)']}
             <br></br>
-            {data['Apple - Fuji(Pink) (1 Kg)']}
+            {data2['Apple - Fuji(Pink) (1 Kg)']}
             <br></br>
-            {data['Arakeerai (1 Bunch)']}
+            {data3['Arakeerai (1 Bunch)']}
             <br></br>
-            {data['Chenna Black ( 1kg )']}
+            {data4['Chenna Black ( 1kg )']}
         </>
     )
 }

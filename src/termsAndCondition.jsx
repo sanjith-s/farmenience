26950a939ec7f4d8ea56f0f944cc2d15e1d354e9
Metta Surendhar <msurendhar8815@gmail.com>
2023-03-28 01:20:07 +0530

    updated the fontsize in tandc page

--- src/termsAndCondition.jsx
+++ src/termsAndCondition.jsx
@@ -21,11 +21,11 @@ const TermsAndCondition = () => {
         <div>
 
           
-            <h2 className="heading-primary" style={{textAlign:"center" , color:"#000",fontWeight:"600"}}>
+            <h2 className="heading-primary" style={{textAlign:"center" , color:"#000",fontWeight:"600",fontSize:"42px"}}>
               Terms and Condition
             </h2>
 
-            <h2 className="heading-tertiary" style={{fontSize:"32px" , fontWeight:"600"}}> Welcome to Farmenience!</h2>
+            <h2 className="heading-tertiary" style={{fontSize:"28px" , fontWeight:"600",marginBottom:"1rem"}}> Welcome to Farmenience!</h2>
 
             <p className="tandc-description " >These terms and conditions outline the rules and regulations for the use of Farmenience's Website, located at <a href="#">Farmenience.com </a></p>
 
@@ -35,7 +35,7 @@ const TermsAndCondition = () => {
             The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: “Client”, “You” and “Your” refers to you, the person log on this website and compliant to the Company's terms and conditions. “The Company”, “Ourselves”, “We”, “Our” and “Us”, refers to our Company. “Party”, “Parties”, or “Us”, refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client's needs in respect of provision of the Company's stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.
             </p>
 
-            <h2 className="heading-tertiary" style={{fontSize:"32px" , fontWeight:"600"}} >
+            <h2 className="heading-tertiary" style={{fontSize:"28px", fontWeight:"600",marginBottom:"1rem"}} >
               Cookies
             </h2>
 
@@ -43,14 +43,14 @@ const TermsAndCondition = () => {
 
             <p className="tandc-description">Most interactive websites use cookies to let us retrieve the user's details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
 
-            <h2 className="heading-tertiary" style={{fontSize:"32px" , fontWeight:"600"}} >
+            <h2 className="heading-tertiary" style={{fontSize:"28px" , fontWeight:"600",marginBottom:"1rem"}} >
               License
             </h2>
 
             <p className="tandc-description">Unless otherwise stated, Farmenience and/or its licensors own the intellectual property rights for all material on Farmenience. All intellectual property rights are reserved. You may access this from Farmenience for your own personal use subjected to restrictions set in these terms and conditions.</p>
             
             
-            <h2 className="heading-tertiary" style={{fontSize:"32px" , fontWeight:"600"}} >
+            <h2 className="heading-tertiary" style={{fontSize:"28px" , fontWeight:"600",marginBottom:"1rem"}} >
             You must not:
             </h2>
 
@@ -70,7 +70,7 @@ const TermsAndCondition = () => {
             Company Name reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions.
             </p>
 
-            <h2 className="heading-tertiary" style={{fontSize:"32px" , fontWeight:"600"}} >
+            <h2 className="heading-tertiary" style={{fontSize:"28px", fontWeight:"600",marginBottom:"1rem"}} >
             You warrant and represent that :
             </h2>
 
@@ -82,7 +82,7 @@ const TermsAndCondition = () => {
             <li>You hereby grant Company Name a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media.</li>
             </ul>
           
-            <h2 className="heading-tertiary" style={{fontSize:"32px" , fontWeight:"600"}} >
+            <h2 className="heading-tertiary" style={{fontSize:"28px" , fontWeight:"600",marginBottom:"1rem"}} >
             Hyperlinking to our Content
             </h2>
 
@@ -95,7 +95,7 @@ const TermsAndCondition = () => {
             Search engines;<br/>
             News organizations;<br/>
             Online directory distributors may link to our Website in the same manner as they hyperlink to the Websites of other listed businesses; and<br/>
-            System wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and charity fundraising groups which may not hyperlink to our Web site.<br/><br/>
+            System wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and charity fundraising groups which may not hyperlink to our Web site.<br/>
             These organizations may link to our home page, to publications or to other Website information so long as the link :<br/> 
             <p className="points">(a) is not in any way deceptive;<br/></p>
             <p className="points">(b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and/or services; and <br/></p>
@@ -113,13 +113,13 @@ const TermsAndCondition = () => {
             online directory distributors;<br/>
             internet portals;<br/>
             accounting, law and consulting firms; and<br/>
-            educational institutions and trade associations.<br/><br/>
+            educational institutions and trade associations.<br/>
             
             We will approve link requests from these organizations if we decide that :<br/> 
             <p className="points">(a) the link would not make us look unfavorably to ourselves or to our accredited businesses;<br/></p>
             <p className="points">(b) the organization does not have any negative records with us; <br/></p>
             <p className="points">(c) the benefit to us from the visibility of the hyperlink compensates the absence of Company Name; and <br/></p> 
-            <p className="points">(d) the link is in the context of general resource information.<br/></p>
+            <p className="points">(d) the link is in the context of general resource information.</p>
 
             These organizations may link to our home page so long as the link : <br/>
             <p className="points">(a) is not in any way deceptive; <br/></p>
@@ -138,10 +138,10 @@ const TermsAndCondition = () => {
             By use of our corporate name; or<br/>
             By use of the uniform resource locator being linked to; or<br/>
             By use of any other description of our Website being linked to that makes sense within the context and format of content on the linking party's site.<br/>
-            No use of Company Name's logo or other artwork will be allowed for linking absent a trademark license agreement.<br/>
+            No use of Company Name's logo or other artwork will be allowed for linking absent a trademark license agreement.
             </p>
             
-            <h2 className="heading-tertiary" style={{fontSize:"32px" , fontWeight:"600"}} >
+            <h2 className="heading-tertiary" style={{fontSize:"28px" , fontWeight:"600",marginBottom:"1rem"}} >
             iFrames
             </h2>
 
@@ -149,7 +149,7 @@ const TermsAndCondition = () => {
             Without prior approval and written permission, you may not create frames around our Webpages that alter in any way the visual presentation or appearance of our Website.
             </p>
 
-            <h2 className="heading-tertiary" style={{fontSize:"32px" , fontWeight:"600"}} >
+            <h2 className="heading-tertiary" style={{fontSize:"28px"  , fontWeight:"600",marginBottom:"1rem"}} >
             Content Liability
             </h2>
 
@@ -157,7 +157,7 @@ const TermsAndCondition = () => {
             We shall not be hold responsible for any content that appears on your Website. You agree to protect and defend us against all claims that is rising on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.
             </p>
 
-            <h2 className="heading-tertiary" style={{fontSize:"32px" , fontWeight:"600"}} >
+            <h2 className="heading-tertiary" style={{fontSize:"28px" , fontWeight:"600",marginBottom:"1rem"}} >
             Reservation of Rights
             </h2>
 
@@ -165,7 +165,7 @@ const TermsAndCondition = () => {
             We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amen these terms and conditions and it's linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions
             </p>
 
-            <h2 className="heading-tertiary" style={{fontSize:"32px" , fontWeight:"600"}} >
+            <h2 className="heading-tertiary" style={{fontSize:"28px" , fontWeight:"600",marginBottom:"1rem"}} >
             Removal of links from our website
             </h2>
 
@@ -177,7 +177,7 @@ const TermsAndCondition = () => {
             We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.
             </p>
 
-            <h2 className="heading-tertiary" style={{fontSize:"32px" , fontWeight:"600"}} >
+            <h2 className="heading-tertiary" style={{fontSize:"28px"  , fontWeight:"600",marginBottom:"1rem"}} >
             Disclaimer
             </h2>
 
@@ -195,9 +195,8 @@ const TermsAndCondition = () => {
             <p className="tandc-description" style={{fontWeight:"500"}}>
             The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer :
             </p>
-            <p className="points">(a) are subject to the preceding paragraph; and <br/><r>
-              </r></p>
-            <p className="points">(b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort and for breach of statutory duty.<br/></p></p>
+            <p className="tandc-points">(a) are subject to the preceding paragraph; and </p>
+            <p className="tandc-points">(b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort and for breach of statutory duty.</p></p>
 
             <p className="tandc-description">
             As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.

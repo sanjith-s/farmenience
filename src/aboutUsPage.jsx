5e6eaaab67f474a2838a6b9509874fe302c55956
Monish25 <81843888+Monish25@users.noreply.github.com>
2023-04-18 12:29:28 +0530

    update to about us page

--- src/aboutUsPage.jsx
+++ src/aboutUsPage.jsx
@@ -38,43 +38,43 @@ const AboutUsPage = () => {
     <div className="about-hero" onClick={(e) => {
       fullAnotherSpeak(e.target.innerText)
     }}>
-      <img className="abt-img" src={abtImg5} alt="" />
+      {/* <img className="abt-img" src={abtImg5} alt="" /> */}
 
-      <main className="about-section">
-        <section>
-          <div id="google_translate_element">
+      {/* <main className="about-section"> */}
+        {/* <section> */}
+          <div>
 
             <div className="gx-d-flex justify-content-center" style={{ position: "relative" }}>
-              <h1 className="heading-primary" style={{ textAlign: "center", color: "#000", fontWeight: "37.5rem", marginBottom: "5rem", fontFamily:"arial" }}>
+              <h1 className="heading-primary" style={{ textAlign: "center", color: "#000", fontWeight: "37.5rem", marginBottom: "5rem", fontFamily:"arial",marginTop:"2%" }}>
                 About Farmenience
               </h1>
               {/* <img src={farmsvg} alt="" style={{ height: "140px", objectFit: "cover", position: "absolute", left: "42%", top: "30px" }} /> */}
             </div>
 
-            <p className="about-description indent">
-              <span style={{ fontSize: "2.5rem" }}>A</span>griculture is the backbone of the Indian Economy’- Mahatma Gandhi. 54.6% of the total workforce is engaged in agriculture and allied sector activities (Census 2011) and accounts for 18.8% (First Advance Estimates) of country's Gross Value Added (GVA) for the year 2021-22 (at current prices).  The resource rich country is yet to be revolutionised by the modern technology. The Farmenience website aims to provide assistance to the farming community to make best use of their resources to grow in a sustainable manner. Farmenience aims to help India to remain an agriculturally dependent economy in the long run. The website makes use of efficient machine learning techniques to predict and provide the farmers the best profitable crop for specific terrain. It acts as a bridge between producers and clients and reduces the intermediate fee by a large factor which in turn benefits the farming community in profit. It acts as a portal which carries the queries of farmers to concerned NGO’s which provides them with guidance.
+            <p className="about-description indent" style={{margin:"80px"}}>
+              <span style={{ fontSize: "2.5rem", }}>A</span>griculture is the backbone of the Indian Economy’- Mahatma Gandhi. 54.6% of the total workforce is engaged in agriculture and allied sector activities (Census 2011) and accounts for 18.8% (First Advance Estimates) of country's Gross Value Added (GVA) for the year 2021-22 (at current prices).  The resource rich country is yet to be revolutionised by the modern technology. The Farmenience website aims to provide assistance to the farming community to make best use of their resources to grow in a sustainable manner. Farmenience aims to help India to remain an agriculturally dependent economy in the long run. The website makes use of efficient machine learning techniques to predict and provide the farmers the best profitable crop for specific terrain. It acts as a bridge between producers and clients and reduces the intermediate fee by a large factor which in turn benefits the farming community in profit. It acts as a portal which carries the queries of farmers to concerned NGO’s which provides them with guidance.
             </p>
 
-            <p className="about-description">
+            <p className="about-description" style={{margin:"80px"}}>
               This website is mainly divided into 2 sections.
 
             </p>
 
-            <p className="about-description points">
+            <p className="about-description points" style={{marginLeft:"170px"}} >
               1.	A professional network component where farm owners can post jobs as per their requirements and the workmen who can accept jobs as per their needs and convenience.
 
             </p>
 
-            <p className="about-description points">
+            <p className="about-description points" style={{marginLeft:"170px"}}>
               2.	A component through which retailers can connect to farmers to buy produce as per their needs. And on the other side, farmers can connect to retailers to sell their produce as per their produce and profitability.
             </p>
 
           </div>
-        </section>
+        {/* </section> */}
 
 
 
-      </main>
+      {/* </main> */}
 
     </div>
   );
